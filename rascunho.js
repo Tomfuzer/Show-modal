f58d61ef844b642abb71d13feabfcb76d3fa784a
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-03-17 16:43:18 -0300

    Refatoração do código e atualização dos textos

---
+++ rascunho.js
@@ -0,0 +1,13 @@
+// modals.addEventListener('click', function () {
+//   console.log('ta pegando');
+// });
+
+showModal0.addEventListener('click', function () {
+  console.log('funcionou!');
+});
+showModal1.addEventListener('click', function () {
+  console.log('funcionou!');
+});
+showModal2.addEventListener('click', function () {
+  console.log('funcionou!');
+});
--- script.js
+++ script.js
@@ -1,34 +1,72 @@
 'use strict';
 
 //Autor: Hamilton Fuzer de Oliveira
+//info
+console.log(
+  'As janelas fecham clicando no X, pressionando a tecla ESC ou clicando em qualquer lugar fora da janela'
+);
 
-const modal = document.querySelector('.modal');
+const modal0 = document.querySelector('.modal0');
+const modal1 = document.querySelector('.modal1');
+const modal2 = document.querySelector('.modal2');
+
 const overlay = document.querySelector('.overlay');
-const btnCloseModal = document.querySelector('.close-modal');
+const btnCloseModal0 = document.querySelector('.close-modal0');
+const btnCloseModal1 = document.querySelector('.close-modal1');
+const btnCloseModal2 = document.querySelector('.close-modal2');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
+const showModal0 = document.querySelector('.show-modal0');
+const showModal1 = document.querySelector('.show-modal1');
+const showModal2 = document.querySelector('.show-modal2');
 
-const openModal = function () {
-  //console.log('Button clicked');
+const openModal = function (n) {
+  const modal = document.querySelector(`.modal${n}`);
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
-const closeModal = function () {
+const closeModal = function (n) {
+  const modal = document.querySelector(`.modal${n}`);
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', openModal);
-}
+//
+showModal0.addEventListener('click', () => {
+  openModal(0);
+});
+showModal1.addEventListener('click', () => {
+  openModal(1);
+});
+showModal2.addEventListener('click', () => {
+  openModal(2);
+});
 
-btnCloseModal.addEventListener('click', closeModal);
-// ('click', closeModal()) não funciona! isso chamaria a função imediatamente.
-overlay.addEventListener('click', closeModal);
+btnCloseModal0.addEventListener('click', () => {
+  closeModal(0);
+});
+btnCloseModal1.addEventListener('click', () => {
+  closeModal(1);
+});
+btnCloseModal2.addEventListener('click', () => {
+  closeModal(2);
+});
+
+overlay.addEventListener('click', () => {
+  closeModal(0);
+  closeModal(1);
+  closeModal(2);
+});
 
 document.addEventListener('keydown', function (e) {
-  //console.log(e.key);
-  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
-    closeModal();
+  if (
+    e.key === 'Escape' &&
+    (!modal0.classList.contains('hidden') ||
+      !modal1.classList.contains('hidden') ||
+      !modal2.classList.contains('hidden'))
+  ) {
+    closeModal(0);
+    closeModal(1);
+    closeModal(2);
   }
 });
